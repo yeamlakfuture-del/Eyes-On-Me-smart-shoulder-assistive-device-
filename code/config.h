ee6f10464a1dcda705ac4192ba6a6775da87d0c5
yeamlakfuture-del <yeamlakfuture@gmail.com>
2026-05-02 20:55:56 +0300

    Create config.h

---
+++ code/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Pins
+#define TRIG_PIN 9
+#define ECHO_PIN 10
+#define MOTOR_PIN 8
+
+// Distance thresholds (cm)
+#define SAFE_DISTANCE 120
+#define WARNING_DISTANCE 80
+#define DANGER_DISTANCE 40
+
+#endif
